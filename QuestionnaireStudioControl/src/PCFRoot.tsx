c9bfa2e1411f0ba11a0701e1ade87b41fdae602c
Pranshu <pranshu@threehumps.com>
2026-01-27 11:03:17 +0530

    Update code

--- QuestionnaireStudioControl/src/PCFRoot.tsx
+++ QuestionnaireStudioControl/src/PCFRoot.tsx
@@ -11,15 +11,6 @@ export const PCFRoot: React.FC<PCFRootProps> = ({ context }) => {
   return (
     <div className="pcf-root">
       <App />
-      {/* <h1 style={{color:"blue",textAlign:"center"}}>This is a heading</h1> */}
     </div>
   );
 };
-
-// export const PCFRoot = () => {
-//   return (
-//     <div style={{ padding: "20px", background: "#d1fae5" }}>
-//       ✅ PCF React is rendering
-//     </div>
-//   );
-// };
